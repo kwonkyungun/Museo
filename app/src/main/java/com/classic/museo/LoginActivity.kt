b7ad3a66bcea60eefacf495c841457c0a749a1be
werds7890 <hihyungul@naver.com>
2023-10-12 18:58:28 +0900

    [PACK][API]API를 활용한 카카오로그인 구현

--- app/src/main/java/com/classic/museo/LoginActivity.kt
+++ app/src/main/java/com/classic/museo/LoginActivity.kt
@@ -1,11 +1,65 @@
 package com.classic.museo
 
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.classic.museo.databinding.ActivityLoginBinding
+import com.classic.museo.databinding.ActivityMainBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "cab9f533e59332f0bbbf15b84459522c")
+    }
+}
 class LoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_longin)
+        setContentView(binding.root)
+
+        binding.kakaoLogin.setOnClickListener{
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("Login", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("Login", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                    val intent= Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e("Login", "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i("Login", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
     }
 }
--- app/src/main/java/com/classic/museo/MainActivity.kt
+++ app/src/main/java/com/classic/museo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.classic.museo
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kakao.sdk.common.KakaoSdk
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
