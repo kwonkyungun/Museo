c2d91f9e1ae9b612a2d64798f0f8f8454ccc470a
cbh1992 <whgg9786@gmail.com>
2023-10-17 20:34:25 +0900

    [FEAT] : MypageFragment 로그아웃 기능 및 작성글, 좋아요 체크 한 글 이동 기능추가 상세사항 코멘트 참조

---
+++ app/src/main/java/com/classic/museo/Fragment/MypageInnerActivity/MypageLike.kt
@@ -0,0 +1,21 @@
+package com.classic.museo.Fragment.MypageInnerActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.classic.museo.R
+import com.classic.museo.databinding.ActivityMypageLikeBinding
+import com.classic.museo.databinding.FragmentMypageBinding
+
+class MypageLike : AppCompatActivity() {
+    lateinit var binding: ActivityMypageLikeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMypageLikeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.MypageLikeBack.setOnClickListener {
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/classic/museo/ViewPager2Adapter.kt
+++ app/src/main/java/com/classic/museo/ViewPager2Adapter.kt
@@ -1,10 +1,9 @@
 package com.classic.museo
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPager2Adapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+class ViewPager2Adapter(fragmentActivity: MainActivity) : FragmentStateAdapter(fragmentActivity) {
 
     var fragments: ArrayList<Fragment> = ArrayList()
 
