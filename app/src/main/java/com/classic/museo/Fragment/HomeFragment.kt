83a55a2556767fbba8a7a950276ff6274eee91a2
kku <ruddns5315@gmail.com>
2023-10-18 09:09:03 +0900

    [FEAT] : ⚙️: 박물관 상세정보 페이지 추가 UI추가

--- app/src/main/java/com/classic/museo/Fragment/HomeFragment.kt
+++ app/src/main/java/com/classic/museo/Fragment/HomeFragment.kt
@@ -7,14 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
-import com.classic.museo.R
+import com.classic.museo.data.Record
 import com.classic.museo.databinding.FragmentHomeBinding
 import com.example.museoapitest.retrofit.NetWorkClient
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding : FragmentHomeBinding
+
+    private lateinit var database : DatabaseReference
+    private var items= mutableListOf<Record>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,12 +40,12 @@ class HomeFragment : Fragment() {
     }
 
     private fun setUpMuseumParameter(): HashMap<String, String> {
-        val authKey = "0OhBU7ZCGIobDVKDeBJDpmDRqK3IRNF6jlf/JB2diFAf/fR2czYO9A4UTGcsOwppV6W2HVUeho/FPwXoL6DwqA=="
+        val authKey = "xGp/squZ8ZOC7m8iSZFc6sAlwRJrKkkPezlcdCwezR4DCKzM2hn4amec4CSXCMQYYU1hGPnKjUAndRsjgLUrZQ=="
 
         return hashMapOf(
             "serviceKey" to authKey,
             "pageNo" to "1",
-            "numOfRows" to "100",
+            "numOfRows" to "2293",
             "type" to "json",
         )
     }
@@ -46,5 +53,13 @@ class HomeFragment : Fragment() {
     private fun communicateNetWork(param: HashMap<String, String>) = lifecycleScope.launch() {
         val responseData = NetWorkClient.museoNetWork.getMuseo(param)
         Log.d("Parsing Museo ::", responseData.toString())
+
+        database= Firebase.database.reference
+        items=responseData.response.museoBody.museoItem!!
+
+        items.forEach {
+            database.child("museumInfo").child("지역별").child("전국").child(it.fcltyNm.replace('.','-')).setValue(it)
+        }
+        Log.e("dbTest","Success")
     }
 }
--- app/src/main/java/com/classic/museo/LoginActivity.kt
+++ app/src/main/java/com/classic/museo/LoginActivity.kt
@@ -4,12 +4,10 @@ import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.util.Log
-import android.widget.EditText
 import android.widget.Toast
+import com.classic.museo.Fragment.DetailActivity
 import com.classic.museo.databinding.ActivityLoginBinding
-import com.classic.museo.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -43,6 +41,7 @@ class LoginActivity : AppCompatActivity() {
 
 
         auth = Firebase.auth // DB 인스턴스 선언
+
 
 
         //로그인 버튼
