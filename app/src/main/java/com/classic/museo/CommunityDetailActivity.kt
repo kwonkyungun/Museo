c57dedfa525599af97eff27986b90db334beb302
cbh1992 <whgg9786@gmail.com>
2023-10-21 00:34:21 +0900

    [FEAT] : [DESIGN] : 상세내역 코멘트 참조

--- app/src/main/java/com/classic/museo/CommunityDetailActivity.kt
+++ app/src/main/java/com/classic/museo/CommunityDetailActivity.kt
@@ -2,10 +2,31 @@ package com.classic.museo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.classic.museo.Fragment.MypageInnerActivity.DummyItem
+import com.classic.museo.Fragment.MypageInnerActivity.LikeAdapter
+import com.classic.museo.databinding.ActivityCommunityDetailBinding
+import com.classic.museo.databinding.ActivityDetailBinding
+import com.google.firebase.ktx.Firebase
 
 class CommunityDetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCommunityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community_detail)
+        binding = ActivityCommunityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //더미데이터
+        val dummy = mutableListOf<DummyItem>()
+        dummy.add(DummyItem("테스트 제목1","테스트 내용1"))
+        dummy.add(DummyItem("테스트 제목2","테스트 내용2"))
+        dummy.add(DummyItem("테스트 제목3","테스트 내용3"))
+        dummy.add(DummyItem("테스트 제목4","테스트 내용4"))
+        binding.recyclerView3.adapter = CommunityDetailItem(this,dummy)
+
+        binding.communityDetailSave.setOnClickListener {
+            Toast.makeText(this,"test",Toast.LENGTH_SHORT).show()
+        }
     }
 }
