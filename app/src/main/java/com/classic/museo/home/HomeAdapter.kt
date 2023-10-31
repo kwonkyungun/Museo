b108b8a8e3f993276329f528ba30853e4b8c948b
werds7890 <hihyungul@naver.com>
2023-11-01 01:19:32 +0900

    [UI][BUG]검색페이지 이미지 수정, 검색 UI수정, 무료박물관/미술관 코드 수정

--- app/src/main/java/com/classic/museo/home/HomeAdapter.kt
+++ app/src/main/java/com/classic/museo/home/HomeAdapter.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.classic.museo.R
 import com.classic.museo.data.Record
 import com.classic.museo.databinding.RecyclerviewItem1Binding
+import com.google.firebase.firestore.Filter
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.GsonBuilder
@@ -66,17 +67,24 @@ class HomeAdapter(
     fun regionDB(text: String) {
         item2.clear()
         fAdapter.clearItem()
-        db.collection("museoInfo")
-            .whereGreaterThanOrEqualTo("rdnmadr","${text}")
-            .whereLessThanOrEqualTo("rdnmadr","${text}" + "\uf8ff").limit(20)
+        val museoRef = db.collection("museoInfo")
+
+        val query = museoRef.whereGreaterThanOrEqualTo("rdnmadr", text)
+            .whereLessThanOrEqualTo("rdnmadr", text + "\uf8ff").limit(50)
+
+        query
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                        val value = gson.toJson(document.data)
-                        val result = gson.fromJson(value, Record::class.java)
-                        item2.add(result)
+                    val value = gson.toJson(document.data)
+                    val result = gson.fromJson(value, Record::class.java)
+                    if(result.etcChrgeInfo=="무료" || result.adultChrge=="0"){
+                        if(result.etcChrgeInfo==""){
+                            item2.add(result)
+                        }
+                    }
                 }
-                fAdapter.freeData=item2
+                fAdapter.freeData = item2
                 fAdapter.notifyDataSetChanged()
             }
             .addOnFailureListener { exception ->
@@ -88,19 +96,19 @@ class HomeAdapter(
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(pos: Int) {
-            binding.subject.text = data.keys.elementAt(pos)
+            binding.subject.text = data.keys.elementAt(pos).toString()
 
-                if(binding.subject.text == "인기" ){
-                    binding.subjectIm.setImageResource(R.drawable.top)
-                }else if(binding.subject.text =="이색 박물관"){
-                    binding.subjectIm.setImageResource(R.drawable.unique)
-                }else if (binding.subject.text =="생물 박물관"){
-                    binding.subjectIm.setImageResource(R.drawable.biology)
-                }else if (binding.subject.text =="과학 박물관"){
-                    binding.subjectIm.setImageResource(R.drawable.science)
-                }else if (binding.subject.text =="무료 박물관/미술관"){
-                    binding.subjectIm.setImageResource(R.drawable.free)
-                }
+            if (binding.subject.text == "인기") {
+                binding.subjectIm.setImageResource(R.drawable.top)
+            } else if (binding.subject.text == "이색 박물관") {
+                binding.subjectIm.setImageResource(R.drawable.unique)
+            } else if (binding.subject.text == "생물 박물관") {
+                binding.subjectIm.setImageResource(R.drawable.biology)
+            } else if (binding.subject.text == "과학 박물관") {
+                binding.subjectIm.setImageResource(R.drawable.science)
+            } else if (binding.subject.text == "무료 박물관/미술관") {
+                binding.subjectIm.setImageResource(R.drawable.free)
+            }
 
 
             if (binding.subject.text == "무료 박물관/미술관") {
@@ -113,7 +121,7 @@ class HomeAdapter(
             adapter = HomeAdapter2(binding.subject.text.toString(), hContext)
             fAdapter = FreeAdapter(hContext)
             if (pos == 4) {
-                Log.e("테스트용","${item2}")
+                Log.e("테스트용", "${item2}")
                 fAdapter.freeData = data.values.elementAt(pos)
                 binding.recyclerHome2.adapter = fAdapter
             } else {
