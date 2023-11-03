ddef8bd540e80b09f3fad46eef42f088e98b555c
werds7890 <hihyungul@naver.com>
2023-11-03 09:46:48 +0900

    [UI]로그인UI, 회원가입UI, 검색UI 수정

---
+++ app/src/main/java/com/classic/museo/itemPage/search/RegionFragment.kt
@@ -0,0 +1,86 @@
+package com.classic.museo.itemPage.search
+
+import android.content.ContentValues
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.classic.museo.R
+import com.classic.museo.data.Record
+import com.classic.museo.databinding.FragmentSearchBinding
+import com.classic.museo.databinding.FragmentSearchRegionBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.gson.GsonBuilder
+
+
+class RegionFragment : Fragment() {
+    private lateinit var binding : FragmentSearchRegionBinding
+    private lateinit var sContext: Context
+    private lateinit var searchAdapter : SearchAdapter
+    private var db = Firebase.firestore
+    private var gson = GsonBuilder().create()
+    private var items = mutableListOf<Record>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sContext=context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentSearchRegionBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialSetUp()
+        binding.searchSpinner.setOnSpinnerItemSelectedListener<String> { _, _, _, text ->
+            binding.searchImage.setOnClickListener{
+                regionSearchDB(text)
+            }
+        }
+    }
+
+    private fun initialSetUp() {
+        searchAdapter = SearchAdapter(sContext)
+        searchAdapter.clearItem()
+        binding.searchRecyclerview.adapter = searchAdapter
+        binding.searchRecyclerview.layoutManager = LinearLayoutManager(sContext)
+        binding.searchRecyclerview.setHasFixedSize(true)
+    }
+
+    private fun regionSearchDB(text: String) {
+        items.clear()
+        searchAdapter.clearItem()
+        db.collection("museoInfo")
+            .whereGreaterThanOrEqualTo("rdnmadr","${text}")
+            .whereLessThanOrEqualTo("rdnmadr","${text}" + "\uf8ff").limit(45)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val value = gson.toJson(document.data)
+                    val result = gson.fromJson(value, Record::class.java)
+                    items.add(result)
+                }
+                searchAdapter.searchItems = items
+                searchAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error", exception)
+            }
+    }
+}
--- app/src/main/java/com/classic/museo/itemPage/search/SearchFragment.kt
+++ app/src/main/java/com/classic/museo/itemPage/search/SearchFragment.kt
@@ -10,9 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.classic.museo.R
 import com.classic.museo.data.Record
 import com.classic.museo.databinding.FragmentSearchBinding
+import com.classic.museo.databinding.FragmentSearchRegionBinding
+import com.classic.museo.databinding.FragmentSearchTabBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.GsonBuilder
@@ -20,15 +24,16 @@ import com.google.gson.GsonBuilder
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private lateinit var sContext: Context
-    private lateinit var searchAdapter: SearchAdapter
     private var db = Firebase.firestore
     private var gson = GsonBuilder().create()
     private var items = mutableListOf<Record>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+        initViewPager()
         return binding.root
     }
 
@@ -37,70 +42,22 @@ class SearchFragment : Fragment() {
         sContext = context
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initialSetUp()
-        binding.searchSpinner.setOnSpinnerItemSelectedListener<String> { _, _, _, text ->
-            binding.searchImage.setOnClickListener{
-                regionSearchDB(text)
-            }
-        }
-        searchTry()
-    }
-
-    private fun searchTry() {
-        binding.searchImage2.setOnClickListener {
-            var text = binding.editTextText3.text.toString()
-            searchAdapter.clearItem()
-            searchOne(text)
-        }
-    }
-
-    private fun initialSetUp() {
-        searchAdapter = SearchAdapter(sContext)
-        binding.searchRecyclerview.adapter = searchAdapter
-        binding.searchRecyclerview.layoutManager = LinearLayoutManager(sContext)
-        binding.searchRecyclerview.setHasFixedSize(true)
-    }
-
-    private fun searchOne(text: String) {
-        searchAdapter.clearItem()
-        val query=db.collection("museoInfo").whereEqualTo("fcltyNm","${text}")
-
-        query.get().addOnSuccessListener { result ->
-                for (document in result) {
-                        val value = gson.toJson(document.data)
-                        val result = gson.fromJson(value, Record::class.java)
-                        items.add(result)
+    private fun initViewPager() {
+        binding.searchViewpager.apply {
+            adapter = ViewPagerAdapter(requireActivity())
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
                 }
-                if(items.isEmpty()){
-                    Toast.makeText(sContext,"찾는 박물관/미술관이 없습니다.",Toast.LENGTH_SHORT).show()
-                }
-                searchAdapter.searchItems = items
-                searchAdapter.notifyDataSetChanged()
-            }.addOnFailureListener { exception ->
-            Log.w(ContentValues.TAG, "Error getting documents.", exception)
+            })
         }
-    }
 
-    private fun regionSearchDB(text: String) {
-        searchAdapter.clearItem()
-        db.collection("museoInfo")
-            .whereGreaterThanOrEqualTo("rdnmadr","${text}")
-            .whereLessThanOrEqualTo("rdnmadr","${text}" + "\uf8ff").limit(45)
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                        val value = gson.toJson(document.data)
-                        val result = gson.fromJson(value, Record::class.java)
-                        items.add(result)
-                }
-                searchAdapter.searchItems = items
-                searchAdapter.notifyDataSetChanged()
+        TabLayoutMediator(binding.tabLayout, binding.searchViewpager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "검색"
+                1 -> tab.text = "지역별 검색"
             }
-            .addOnFailureListener { exception ->
-                Log.w(ContentValues.TAG, "Error", exception)
-            }
+        }.attach()
     }
 
 
